b12402650a73e74b14daaca461703a33633ef89d
danil348 <eliseevdanil348@gmail.com>
2022-10-23 16:29:02 +0300

    3 timer states

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { TimerComponent } from './components/timer/timer.component';
+import { TimerSettingsComponent } from './components/timer/components/timer-settings/timer-settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimerComponent,
+    TimerSettingsComponent,
   ],
   imports: [
     BrowserModule
@@ -13,4 +16,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
